08549bc0e3c37e5bcd85f215dd7741f5543a48df
jwlee <doyeongk99@gmail.com>
2021-01-29 10:50:59 +0900

    프레임워크와 라이브러리 스프링 프레임워크의 탄생 배경
    IOC 컨테이너
    
    IOC 컨테이너 생성
     - sts3 플러그인 설치
     - 스프링 설정파일(스프링 빈 생성) 작성
     - scope 테스트(singleton, prototype)
     - collection 객체 생성
     - property-placeholder
     - java config

---
+++ src/test/java/kr/or/ddit/user/service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package kr.or.ddit.user.service;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.user.model.UserVo;
+import kr.or.ddit.user.service.UserService;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/kr/or/ddit/ioc/ioc.xml")
+
+public class UserServiceTest {
+	
+	@Resource(name="userService")
+	private UserService userSerivce;
+	
+	@Test 
+	public void getUserTest() {
+		/***Given***/
+		String userid = "brown";
+
+		/***When***/
+        UserVo userVo = userSerivce.getUser(userid);
+		
+        /***Then***/
+		assertEquals("ºê¶ó¿î", userVo.getUsernm());
+	}
+
+}
